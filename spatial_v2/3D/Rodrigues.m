ede6693bbe0457ab757274d7672a848c0c9df4c2
Iram Munoz <iram.munoz@cinvestav.edu.mx>
2023-06-14 19:08:48 -0600

    Add files via upload

---
+++ spatial_v2/3D/Rodrigues.m
@@ -0,0 +1,19 @@
+function [X,S] = Rodrigues(w,dt)
+% w should be a vector of size 3
+
+norm_w = norm(w);
+if norm_w < eps
+    R = eye(3);
+else
+    wn = w/norm_w;   % rotation axis (unit vector)
+    th = norm_w*dt;  % amount of rotation (rad)
+%     w_wedge = [0 -wn(3) wn(2);wn(3) 0 -wn(1);-wn(2) wn(1) 0];
+    % transposed w_wedge
+    w_wedge = [0 wn(3) -wn(2);-wn(3) 0 wn(1);wn(2) -wn(1) 0];
+    R = eye(3) + w_wedge * sin(th) + w_wedge^2 * (1-cos(th));
+end
+
+S = zeros(6,1);
+S(1:3,1) = w;
+
+X = [R,zeros(3);zeros(3),R];
